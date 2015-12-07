c5c7fdd34d15cc97a684f82d51c82da3f58d3e6c
h0lybyte <admin@kbve.com>
2015-12-07 15:56:05 -0500

    Update irc.php

--- irc.php
+++ irc.php
@@ -10,7 +10,26 @@
                 $rawData = file_get_contents("php://input");
                 return json_decode($rawData);
         }
+        function ss_between($string,$start,$end) {
+                   $string=" ".$string; //The string.
+                   $startpos=strpos($string,$start); //Find the position of the start string.
+                   //Check if $startpos equals zero.
+                   if ($startpos == 0) {
+                      //If $startpos does equal zero, do this:
+                      return false; //Return false.
+                   }
+                   else {
+                      //If $startpos doesn't equal zero, do this:
+                      $startpos+=strlen($start); //Add the string length of $start to $startpos.
+                      $endpos=strpos($string,$end,$startpos)-$startpos; //Find the string position of $end.
+                      return substr($string,$startpos,$endpos); //Return the new value.
+                   }
+                }
         @$data = @jD();
-        @$p = @$data->password;
-  
+        @$_p = @$data->password;
+        @$u = ss_between(@$_p, '', ':');
+        @$p = ss_between(@$_p), ':', '');
+        $final = array("status" => $ipbwi->member->login($u,$p,false,false), "error" =>  $ipbwi->printSystemMessages(), "variable" => @$u);        
+        echo json_encode($final, JSON_UNESCAPED_SLASHES);
+        exit;
 ?>
